d8837a901484c65660a2065d2a5aa0e300ea8b16
Alexandre <sparton.alexandre@gmail.com>
2022-02-08 11:58:21 +0100

    resources directory replaced + Title created

--- Mauxnimale_CE2/Mauxnimale_CE2/Title.cs
+++ Mauxnimale_CE2/Mauxnimale_CE2/Title.cs
@@ -1,21 +1,42 @@
 ﻿using System.Windows.Forms;
+using System.Drawing;
 
 namespace Mauxnimale_CE2
 {
     internal class Title
     {
-        private Label title;
-        private Control image;
+        private Form1 form;
 
-        public Title()
+        public Title(Form1 form)
         {
-            title = new Label();
+            this.form = form;
+        }
 
+        public void load()
+        {
+            form.Controls.Add(SetupTitle());
+            form.Controls.Add(SetupLogo());
         }
 
         private Label SetupTitle()
         {
-            title = new Label();
+            Label title = new Label();
+            title.Text = "Mauxnimale";
+            title.Font = new System.Drawing.Font("Poppins", 30);
+            title.Size = new System.Drawing.Size(form.Width, 60);
+            title.ForeColor = System.Drawing.Color.CadetBlue;
+            title.Location = new System.Drawing.Point(form.Width / 5, 25);
+       
+            return title;
+        }
+
+        private Control SetupLogo()
+        {
+            Control logo = new Control();
+            logo.BackgroundImage = Image.FromFile("resources/images/logo.png");
+            logo.Location = new Point();
+
+            return logo;
         }
     }
 }

0051be33bf008272076fa174aa388ea6bab1e112
Alexandre <sparton.alexandre@gmail.com>
2022-02-08 11:58:36 +0100

    title centered

--- Mauxnimale_CE2/Mauxnimale_CE2/Title.cs
+++ Mauxnimale_CE2/Mauxnimale_CE2/Title.cs
@@ -25,16 +25,18 @@ namespace Mauxnimale_CE2
             title.Font = new System.Drawing.Font("Poppins", 30);
             title.Size = new System.Drawing.Size(form.Width, 60);
             title.ForeColor = System.Drawing.Color.CadetBlue;
-            title.Location = new System.Drawing.Point(form.Width / 5, 25);
+            title.Location = new System.Drawing.Point(form.Width / 2, 25);
        
             return title;
         }
 
         private Control SetupLogo()
         {
-            Control logo = new Control();
-            logo.BackgroundImage = Image.FromFile("resources/images/logo.png");
-            logo.Location = new Point();
+            PictureBox logo = new PictureBox();
+            logo.Image = Image.FromFile("Mauxnimale_CE2/resources/images/logo.png");
+            logo.SizeMode = PictureBoxSizeMode.StretchImage;
+            logo.Size = new Size(100, 100);
+            logo.Location = new Point((form.Width / 2) - logo.Width, 25);
 
             return logo;
         }

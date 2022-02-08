8f12f980f66d48dd48665b94ee5fc312de4d695c
Hugo Faure <hugofaure52@gmail.com>
2022-02-08 09:06:47 +0100

    Mise en place du window manager fini

--- Mauxnimale_CE2/Mauxnimale_CE2/Form1.cs
+++ Mauxnimale_CE2/Mauxnimale_CE2/Form1.cs
@@ -12,9 +12,21 @@ namespace Mauxnimale_CE2
 {
     public partial class Form1 : Form
     {
+        InterfaceAbs interfac; //pas interface parce que sinon ca marche mal
         public Form1()
         {
             InitializeComponent();
+
+            //interfac = new InterfaceConnexion(this);
+            //interfac.load();
+
+            Refresh();
+        }
+
+        public void switchInterface(InterfaceAbs interf)
+        {
+            interfac = interf;
+            interfac.load();
         }
     }
 }

f87724035d68dffc14c531a6e5e8878e85967743
Alexandre <sparton.alexandre@gmail.com>
2022-02-08 11:18:02 +0100

    class created

---
+++ Mauxnimale_CE2/Mauxnimale_CE2/Title.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+
+namespace Mauxnimale_CE2
+{
+    internal class Title
+    {
+        private Label title;
+        private Control image;
+
+        public Title()
+        {
+            title = new Label();
+
+        }
+
+        private Label SetupTitle()
+        {
+            title = new Label();
+        }
+    }
+}

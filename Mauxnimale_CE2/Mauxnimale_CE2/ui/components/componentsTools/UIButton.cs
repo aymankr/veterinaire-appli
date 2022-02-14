651a37c674d8178bc7bbdcba09265e375544b208
Hugo Faure <hugofaure52@gmail.com>
2022-02-14 14:11:01 +0100

    ui boutton

---
+++ Mauxnimale_CE2/Mauxnimale_CE2/ui/components/componentsTools/UIButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mauxnimale_CE2.ui.components.componentsTools
+{
+    public partial class UIButton : Button
+    {
+        public UIButton(Color color, string text, int width)
+        {
+            BackColor = color;
+            Text = text;
+            ForeColor = Color.White;
+            FlatStyle = FlatStyle.Flat;
+            FlatAppearance.BorderSize = 0;
+            Font = new System.Drawing.Font("Poppins", width / 8);
+            Size = new Size(width, width / 3);
+        }
+    }
+}

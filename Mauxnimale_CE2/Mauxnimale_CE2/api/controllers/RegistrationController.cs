8a95a205722598c5fc57641ff687dc3c5734eff0
Alexandre <sparton.alexandre@gmail.com>
2022-02-27 17:31:04 +0300

    user already exists & registration controller

---
+++ Mauxnimale_CE2/Mauxnimale_CE2/api/controllers/RegistrationController.cs
@@ -0,0 +1,30 @@
+﻿using Mauxnimale_CE2.api.entities;
+
+namespace Mauxnimale_CE2.api.controllers
+{
+    /// <summary>
+    /// Static class implementing the users registration related methods.
+    /// </summary>
+    internal static class RegistrationController
+    {
+        /// <summary>
+        /// Verify if the given user already exists in the current context.
+        /// </summary>
+        /// <param name="userToVerify">The user to verify</param>
+        /// <returns>true if the user has the same login, email or phone number as one user in the current context, false otherwise.</returns>
+        public static bool userAlreadyExists(SALARIE userToVerify)
+        {
+            PT4_S4P2C_E2Entities dbAccessor = new PT4_S4P2C_E2Entities();
+
+            foreach (SALARIE registeredUser in dbAccessor.SALARIE)
+            {
+                if (registeredUser.LOGIN.Equals(userToVerify.LOGIN) ||
+                    registeredUser.EMAIL.Equals(userToVerify.EMAIL) ||
+                    registeredUser.TEL.Equals(userToVerify.TEL))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}

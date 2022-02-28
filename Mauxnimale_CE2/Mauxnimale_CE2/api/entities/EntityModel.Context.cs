0a16281b2608c791ed4f499ddae0cefff3f5f9e1
Ayman Kr <kr.ayman@hotmail.com>
2022-02-28 10:08:12 +0100

    updating orm

--- Mauxnimale_CE2/Mauxnimale_CE2/api/entities/EntityModel.Context.cs
+++ Mauxnimale_CE2/Mauxnimale_CE2/api/entities/EntityModel.Context.cs
@@ -13,10 +13,10 @@ namespace Mauxnimale_CE2.api.entities
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class Entities : DbContext
+    public partial class PT4_S4P2C_E2Entities : DbContext
     {
-        public Entities()
-            : base("name=Entities")
+        public PT4_S4P2C_E2Entities()
+            : base("name=PT4_S4P2C_E2Entities")
         {
         }
     

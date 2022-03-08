4b7a5e54b1ae5166574094b6995735d92c167b90
Grégory Simon <gregory.simon@etu.u-bordeaux.fr>
2022-03-08 15:40:06 +0100

    ajout de l'option ajouter client

---
+++ Mauxnimale_CE2/Mauxnimale_CE2/api/controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mauxnimale_CE2.api.entities;
+
+namespace Mauxnimale_CE2.api.controllers
+{
+    internal static class ClientController
+    {
+       public static void addClient(string name, string surname, string phoneNumber)
+        {
+            CLIENT c = new CLIENT();
+            c.NOMCLIENT = surname;
+            c.PRENOMCLIENT = name;
+            c.TELCLIENT = phoneNumber;
+            DbContext.get().CLIENT.Add(c);
+            DbContext.get().SaveChanges();
+        }
+    }
+}

5a2c514fcb4ebab85b9aa9c8657b742625baf46b
Alexandre <sparton.alexandre@gmail.com>
2022-03-08 17:44:33 +0100

    can modify anything you want concerning a SALARIE

--- Mauxnimale_CE2/Mauxnimale_CE2/ui/InterfaceConnection.cs
+++ Mauxnimale_CE2/Mauxnimale_CE2/ui/InterfaceConnection.cs
@@ -80,10 +80,10 @@ namespace Mauxnimale_CE2
 
         public void connection_click(object sender, EventArgs e)
         {
-            if (login.Text != null && password.Text != null && ConnectionController.getUser(login.Text, password.Text) != null)
+            if (login.Text != null && password.Text != null && ConnectionController.getConnection(login.Text, password.Text) != null)
             {
                 form.Controls.Clear();
-                form.switchInterface(new InterfaceHome(form, ConnectionController.getUser(login.Text, password.Text)));
+                form.switchInterface(new InterfaceHome(form, ConnectionController.getConnection(login.Text, password.Text)));
             } else
             {
                 MessageBox.Show("identifiant ou mot de passe incorrecte");

c042c185560fac09b3d983e6f1120eb93cd18bc5
Alexandre <sparton.alexandre@gmail.com>
2022-03-08 15:00:37 +0100

    assistant can be created with temporary hased mdp

--- Mauxnimale_CE2/Mauxnimale_CE2/api/controllers/RegistrationController.cs
+++ Mauxnimale_CE2/Mauxnimale_CE2/api/controllers/RegistrationController.cs
@@ -1,30 +1,109 @@
-﻿using Mauxnimale_CE2.api.entities;
+﻿using System;
+
+using System.Security.Cryptography;
+using System.Text;
+
+using System.Linq;
+using Mauxnimale_CE2.api.entities;
 
 namespace Mauxnimale_CE2.api.controllers
 {
-    /// <summary>
-    /// Static class implementing the users registration related methods.
-    /// </summary>
-    internal static class RegistrationController
+    internal class RegistrationController
     {
         /// <summary>
-        /// Verify if the given user already exists in the current context.
+        /// Creates and returns the hash of the given text.
         /// </summary>
-        /// <param name="userToVerify">The user to verify</param>
-        /// <returns>true if the user has the same login, email or phone number as one user in the current context, false otherwise.</returns>
-        public static bool userAlreadyExists(SALARIE userToVerify)
+        /// <param name="text">The text to get the hash</param>
+        /// <returns>the hash of the given text</returns>
+        private static string getHash(string text)
         {
-            PT4_S4P2C_E2Entities dbAccessor = new PT4_S4P2C_E2Entities();
-
-            foreach (SALARIE registeredUser in dbAccessor.SALARIE)
+            // SHA512 is disposable by inheritance.  
+            using (var sha256 = SHA256.Create())
             {
-                if (registeredUser.LOGIN.Equals(userToVerify.LOGIN) ||
-                    registeredUser.EMAIL.Equals(userToVerify.EMAIL) ||
-                    registeredUser.TEL.Equals(userToVerify.TEL))
-                    return true;
+                // Send a sample text to hash.  
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+                // Get the hashed string.  
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
+
+        /// <summary>
+        /// Generates a random string with a given size.
+        /// </summary>
+        /// <param name="size">The size of the string to generate.</param>
+        /// <param name="lowerCase">True to generate a lower case string.</param>
+        /// <returns>The generated string.</returns>
+        private static string generateRandomString(int size = 10, bool lowerCase = false)
+        {
+            var builder = new StringBuilder(size);
+            Random randomizer = new Random();
+
+            // Unicode/ASCII Letters are divided into two blocks
+            // (Letters 65–90 / 97–122):
+            // The first group containing the uppercase letters and
+            // the second group containing the lowercase.  
+
+            // char is a single Unicode character  
+            char offset = lowerCase ? 'a' : 'A';
+            const int lettersOffset = 26; // A...Z or a..z: length=26  
+
+            for (var i = 0; i < size; i++)
+            {
+                var @char = (char)randomizer.Next(offset, offset + lettersOffset);
+                builder.Append(@char);
             }
 
-            return false;
+            return lowerCase ? builder.ToString().ToLower() : builder.ToString();
+        }
+
+        /// <summary>
+        /// Verify if the a user with the given login already exists in the database or not.
+        /// </summary>
+        /// <param name="login">The login to verify</param>
+        /// <returns>true if a user with the given login already exists, false otherwise.</returns>
+        private static bool userWithLoginAlreadyExists(string login)
+        {
+            PT4_S4P2C_E2Entities dbContext = DbContext.get();
+
+            var userWithLogin = from user in dbContext.SALARIE
+                                where user.LOGIN == login
+                                select user;
+
+            return userWithLogin.Any();
+        }
+
+        public static bool registerNewUser(string newUserLogin, out string tempHashedPassword)
+        {
+            // Verify that the login does not already exists
+            if (userWithLoginAlreadyExists(newUserLogin))
+            {
+                Console.WriteLine("User with login: " + newUserLogin + " already exists.");
+                tempHashedPassword = null;
+                return false;
+            }
+
+            // Generate a temporary password
+            tempHashedPassword = getHash(generateRandomString());
+
+            // Create the new user without the informations (will be modified by him and Annie Maux later)
+            SALARIE newUser = new SALARIE();
+            newUser.LOGIN = newUserLogin;
+            newUser.MDP = tempHashedPassword;
+            newUser.NOM = "None";
+            newUser.PRENOM = "None";
+            newUser.SALAIRE = -1;
+            newUser.ADRESSE = "None";
+            newUser.EMAIL = "None";
+            newUser.TEL = "None";
+            newUser.ASSISTANT = true;
+            newUser.PREMIERECONNEXION = false;
+
+            // Add the create user in db
+            PT4_S4P2C_E2Entities dbContext = DbContext.get();
+            dbContext.SALARIE.Add(newUser);
+            dbContext.SaveChanges();
+
+            return true;
         }
     }
 }

dd909bbd24c519c8bf9a729b8858c83dfd089ea3
Ayman Kr <kr.ayman@hotmail.com>
2022-03-14 08:53:06 +0100

    create an invoice/facture

--- Mauxnimale_CE2/Mauxnimale_CE2/api/controllers/InvoiceController.cs
+++ Mauxnimale_CE2/Mauxnimale_CE2/api/controllers/InvoiceController.cs
@@ -9,12 +9,13 @@ namespace Mauxnimale_CE2.api.controllers
 {
     public static class InvoiceController
     {
-        public static void addInvoice(CLIENT customer, DateTime date, decimal price)
+        public static void addInvoice(CLIENT customer, DateTime date, decimal price, ICollection<PRODUITVENDU> products)
         {
             FACTURE_PRODUIT invoice = new FACTURE_PRODUIT();
             invoice.CLIENT = customer;
             invoice.DATE = date;
             invoice.MONTANT = price;
+            invoice.PRODUITVENDU = products;
             DbContext.get().FACTURE_PRODUIT.Add(invoice);
             DbContext.get().SaveChanges();
         }

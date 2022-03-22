f7c790df16b4401efc4fc5007f3f7ee5fd62983d
Hugo Faure <hugofaure52@gmail.com>
2022-03-22 17:24:01 +0100

    interface fini

--- Mauxnimale_CE2/Mauxnimale_CE2/ui/InterfaceStockNewProduct.cs
+++ Mauxnimale_CE2/Mauxnimale_CE2/ui/InterfaceStockNewProduct.cs
@@ -1,4 +1,5 @@
-﻿using Mauxnimale_CE2.api.entities;
+﻿using Mauxnimale_CE2.api.controllers;
+using Mauxnimale_CE2.api.entities;
 using Mauxnimale_CE2.ui.components;
 using Mauxnimale_CE2.ui.components.componentsTools;
 using System;
@@ -19,11 +20,33 @@ namespace Mauxnimale_CE2.ui
         Footer footer;
 
         ComboBox type;
-        List<TYPE_PRODUIT> productType = new List<TYPE_PRODUIT>();
+        TYPE_PRODUIT choosed;
+        UIRoundButton back, home;
+        UIButton addProduct;
 
-        TextBox quantity, name, initialCost, resellCost, expirationDate;
+        NumericUpDown quantity, initialCost, resellCost;
+        Label lQuantity, lInitialCost, lResellCost, lExpirationDate, lName, lType;
+        DateTimePicker expirationDate;
+        TextBox name;
 
         #region Input events handling
+
+        public void backClick(object sender, EventArgs e)
+        {
+            window.Controls.Clear();
+            window.switchInterface(new InterfaceStockManagement(window, user));
+        }
+
+        public void newProductClick(object sender, EventArgs e)
+        {
+            //Ajouter le produit a la base;
+        }
+
+        public void homeClick(object sender, EventArgs e)
+        {
+            window.Controls.Clear();
+            window.switchInterface(new InterfaceHome(window, user));
+        }
         private void lastNameEnter(object sender, EventArgs e)
         {
             if (quantity.Text == "Nom")
@@ -35,13 +58,7 @@ namespace Mauxnimale_CE2.ui
 
         public void typeSelectedChange(object sender, EventArgs e)
         {
-            //mettre a jour la liste des produits (productsList) a affichés en fonction du type choisi (type.items[type.SelectedIndex])
-        }
-
-        public void productSelectedChange(object sender, EventArgs e)
-        {
-            //choisi = Le produit qui faut chopper a partir du string (product.items[product.SelectedIndex])
-            //si jamais ca vous casse les couilles de chopper le produit a partir d'un string appelez moi je vous dirai comment modifiez la listBox pour que ca soit plus simple
+            choosed = (TYPE_PRODUIT)type.Items[type.SelectedIndex];
         }
 
 
@@ -54,81 +71,73 @@ namespace Mauxnimale_CE2.ui
             }
         }
 
-        private void firstNameEnter(object sender, EventArgs e)
+        private void nameEnter(object sender, EventArgs e)
         {
-            if (name.Text == "Prénom")
+            if (name.Text == "Nom")
             {
                 name.Text = "";
                 name.ForeColor = Color.Black;
             }
         }
 
-        private void firstNameLeave(object sender, EventArgs e)
+        private void nameLeave(object sender, EventArgs e)
         {
             if (name.Text.Length == 0)
             {
-                name.Text = "Prénom";
+                name.Text = "Nom";
                 name.ForeColor = Color.Gray;
             }
         }
+        #endregion
 
-        private void emailEnter(object sender, EventArgs e)
+        public void generateLabel()
         {
-            if (expirationDate.Text == "Email")
-            {
-                expirationDate.Text = "";
-                expirationDate.ForeColor = Color.Black;
-            }
-        }
-
-        private void emailLeave(object sender, EventArgs e)
-        {
-            if (expirationDate.Text.Length == 0)
-            {
-                expirationDate.Text = "Email";
-                expirationDate.ForeColor = Color.Gray;
-            }
-        }
-
-        private void pswdEnter(object sender, EventArgs e)
-        {
-            if (initialCost.Text == "Mot de passe")
-            {
-                initialCost.Text = "";
-                initialCost.ForeColor = Color.Black;
-                initialCost.PasswordChar = '•';
-            }
+            lQuantity = new Label();
+            lQuantity.Location = new Point(window.Width / 20, window.Height * 42 / 100);
+            setLabel(lQuantity, "Quantité");
+            lInitialCost = new Label();
+            lInitialCost.Location = new Point(window.Width / 20, window.Height * 52 / 100);
+            setLabel(lInitialCost, "Prix d'achat");
+            lResellCost = new Label();
+            lResellCost.Location = new Point(window.Width / 20, window.Height * 62 / 100);
+            setLabel(lResellCost, "Prix de revente");
+            lExpirationDate = new Label();
+            lExpirationDate.Location = new Point(window.Width / 20, window.Height * 22 / 100);
+            setLabel(lExpirationDate, "Date d'expiration");
+            lName = new Label();
+            lName.Location = new Point(window.Width / 20, window.Height * 12 / 100);
+            setLabel(lName, "Nom");
+            lType = new Label();
+            lType.Location = new Point(window.Width / 20, window.Height / 3);
+            setLabel(lType, "Type");
         }
 
-        private void pswdLeave(object sender, EventArgs e)
+        public void generateButton()
         {
-            if (initialCost.Text.Length == 0)
-            {
-                initialCost.Text = "Mot de passe";
-                initialCost.ForeColor = Color.Gray;
-                initialCost.PasswordChar = (char)0;
-            }
-        }
-        private void confirmPswdEnter(object sender, EventArgs e)
-        {
-            if (resellCost.Text == "Confirmation du mot de passe")
-            {
-                resellCost.Text = "";
-                resellCost.ForeColor = Color.Black;
-                resellCost.PasswordChar = '•';
-            }
+            addProduct = new UIButton(UIColor.ORANGE, "Nouveau Produit", Math.Min(window.Width / 4, window.Height / 3));
+            addProduct.Location = new System.Drawing.Point(window.Width * 2 / 5, window.Height * 75 / 100);
+            addProduct.Click += new EventHandler(newProductClick);
+            window.Controls.Add(addProduct);
+
+            back = new UIRoundButton(window.Width / 20, "<");
+            back.Location = new System.Drawing.Point(window.Width * 9 / 10, window.Height / 10);
+            back.Click += new EventHandler(backClick);
+            window.Controls.Add(back);
+
+            home = new UIRoundButton(window.Width / 20, "«");
+            home.Location = new System.Drawing.Point(window.Width * 8 / 10, window.Height / 10);
+            home.Click += new EventHandler(homeClick);
+            window.Controls.Add(home);
         }
 
-        private void confirmPswdLeave(object sender, EventArgs e)
+        public void setLabel(Label l, string s)
         {
-            if (resellCost.Text.Length == 0)
-            {
-                resellCost.Text = "Confirmation du mot de passe";
-                resellCost.ForeColor = Color.Gray;
-                resellCost.PasswordChar = (char)0;
-            }
+            l.Font = new System.Drawing.Font("Poppins", window.Height * 2 / 100);
+            l.ForeColor = UIColor.LIGHTBLUE;
+            l.Text = s;
+            l.Size = new System.Drawing.Size(window.Width / 3, window.Height * 6 / 100);
+            window.Controls.Add(l);
         }
-        #endregion 
 
         public InterfaceStockNewProduct(MainWindow window, SALARIE user)
         {
@@ -141,12 +150,12 @@ namespace Mauxnimale_CE2.ui
         private void generateBox()
         {
             type = new ComboBox();
-            //génerer liste des types avec une fonction API requete sql tous ca
-            foreach (TYPE_PRODUIT typeProduct in productType)
+            foreach (TYPE_PRODUIT typeProduct in ProductController.getTypes())
             {
                 type.Items.Add(typeProduct);
             }
             type.Size = new System.Drawing.Size(window.Width / 2, window.Height / 7);
+            type.DropDownStyle = ComboBoxStyle.DropDownList;
             type.Font = new System.Drawing.Font("Poppins", window.Height / 40);
             type.Location = new Point(window.Width / 4, window.Height * 5 / 15);
             type.ForeColor = Color.Gray;
@@ -160,46 +169,65 @@ namespace Mauxnimale_CE2.ui
             footer.load();
             generateTextBoxes();
             generateBox();
+            generateNumUpDown();
+            generateDatePicker();
+            generateButton();
+            generateLabel();
+        }
+
+        public void generateDatePicker()
+        {
+            expirationDate = new DateTimePicker();
+            expirationDate.Format = DateTimePickerFormat.Custom;
+            expirationDate.CustomFormat = "dd:MM";
+            expirationDate.Size = new Size(window.Width / 2, window.Height * 5 / 100);
+            expirationDate.Location = new Point(window.Width / 4, window.Height * 22 / 100);
+            expirationDate.Font = new System.Drawing.Font("Poppins", window.Height * 3 / 100);
+            expirationDate.ForeColor = Color.Black;
+            window.Controls.Add(expirationDate);
         }
 
         public void generateTextBoxes()
         {
-            quantity = new TextBox();
-            quantity.LostFocus += new EventHandler(lastNameLeave);
-            quantity.GotFocus += new EventHandler(lastNameEnter);
-            quantity.Size = new Size(window.Width / 4, quantity.Height);
-            quantity.Location = new Point(window.Width / 2 - quantity.Width - 10, window.Height * 12 / 100);
-            setBox(quantity, "Quantité");
-
             name = new TextBox();
-            name.LostFocus += new EventHandler(firstNameLeave);
-            name.GotFocus += new EventHandler(firstNameEnter);
-            name.Size = new Size(window.Width / 4, quantity.Height);
-            name.Location = new Point(window.Width / 2 + 10, window.Height * 12 / 100);
-            setBox(name, "Nom");
+            name.LostFocus += new EventHandler(nameLeave);
+            name.GotFocus += new EventHandler(nameEnter);
+            name.Size = new Size(window.Width / 2, window.Height * 5 / 100);
+            name.Location = new Point(window.Width / 4, window.Height * 12 / 100);
+            name.Font = new System.Drawing.Font("Poppins", window.Height * 3 / 100);
+            name.ForeColor = Color.Gray;
+            name.Text = "Nom";
+            window.Controls.Add(name);
+        }
 
-            expirationDate = new TextBox();
-            expirationDate.LostFocus += new EventHandler(emailLeave);
-            expirationDate.GotFocus += new EventHandler(emailEnter);
-            expirationDate.Size = new Size(window.Width / 2 + 20, window.Height * 5 / 100);
-            expirationDate.Location = new Point(window.Width / 4 - 10, window.Height * 22 / 100);
-            setBox(expirationDate, "Date D'expiration");
+        public void generateNumUpDown()
+        {
+            initialCost = new NumericUpDown();
+            initialCost.Size = new Size(window.Width / 2, window.Height * 5 / 100);
+            initialCost.Location = new Point(window.Width / 4, window.Height * 52 / 100);
+            initialCost.Font = new System.Drawing.Font("Poppins", window.Height * 3 / 100);
+            initialCost.ForeColor = Color.Black;
+            initialCost.Minimum = 0;
+            initialCost.Maximum = 100000;
+            window.Controls.Add(initialCost);
 
-            initialCost = new TextBox();
-            initialCost.LostFocus += new EventHandler(pswdLeave);
-            initialCost.GotFocus += new EventHandler(pswdEnter);
-            initialCost.Size = new Size(window.Width / 2 + 20, window.Height * 5 / 100);
-            initialCost.Location = new Point(window.Width / 4 - 10, window.Height * 52 / 100);
-            initialCost.PasswordChar = (char)0;
-            setBox(initialCost, "Prix d'achat");
+            resellCost = new NumericUpDown();
+            resellCost.Size = new Size(window.Width / 2, window.Height * 5 / 100);
+            resellCost.Location = new Point(window.Width / 4, window.Height * 62 / 100);
+            resellCost.Font = new System.Drawing.Font("Poppins", window.Height * 3 / 100);
+            resellCost.ForeColor = Color.Black;
+            resellCost.Minimum = 0;
+            resellCost.Maximum = 100000;
+            window.Controls.Add(resellCost);
 
-            resellCost = new TextBox();
-            resellCost.LostFocus += new EventHandler(confirmPswdLeave);
-            resellCost.GotFocus += new EventHandler(confirmPswdEnter);
-            resellCost.Size = new Size(window.Width / 2 + 20, window.Height * 5 / 100);
-            resellCost.Location = new Point(window.Width / 4 - 10, window.Height * 62 / 100);
-            resellCost.PasswordChar = (char)0;
-            setBox(resellCost, "Prix de reventes");
+            quantity = new NumericUpDown();
+            quantity.Size = new Size(window.Width / 2, quantity.Height);
+            quantity.Location = new Point(window.Width / 4, window.Height * 42 / 100);
+            quantity.Font = new System.Drawing.Font("Poppins", window.Height * 3 / 100);
+            quantity.ForeColor = Color.Black;
+            quantity.Minimum = 0;
+            quantity.Maximum = 100000;
+            window.Controls.Add(quantity);
         }
 
         public void setBox(TextBox textBox, string text)
@@ -212,7 +240,8 @@ namespace Mauxnimale_CE2.ui
 
         public override void updateSize()
         {
-            //throw new NotImplementedException();
+            window.Controls.Clear();
+            this.load();
         }
     }
 }
